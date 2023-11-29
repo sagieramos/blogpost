0cd71715641e57727f70cd10f903320212936913
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-11-29 03:18:16 +0100

    added post_counter colume to user table

--- db/migrate/20231129011739_create_users.rb
+++ db/migrate/20231129011739_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration[7.1]
       t.string :name, null: false
       t.string :photo, null: false
       t.text :bio
+      t.integer :posts_counter, null: false, default: 0
 
       # Add an index for the photo column
       t.index :photo, unique: true
