d76c1fabed15d4ecefcb29d626be5e73c0c35b8a
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-12-01 09:01:47 +0100

    testing display comments and post

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,4 +17,16 @@ RSpec.describe User, type: :model do
     subject.posts_counter = -1
     expect(subject).to_not(be_valid)
   end
+
+  it 'should have a positive posts counter' do
+    subject.posts_counter = -1
+    expect(subject).to_not(be_valid)
+  end
+
+  it 'displays three recent posts when called' do
+    posts = 4.times.map { |i| subject.posts.create(title: "Post #{i}") }
+
+    expect(subject.recent_posts.length).to eq(3)
+    expect(subject.recent_posts).to eq(posts.last(3).reverse)
+  end
 end
