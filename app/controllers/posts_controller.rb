f51989ed9d885e653ac393e6b94013c6af940169
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-12-07 00:43:06 +0100

    users#745#posts#:id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
-  def index; end
-  def show; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
   # get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
   # get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index]
+  end
 end
