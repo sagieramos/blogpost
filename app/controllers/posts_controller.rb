ee5aae56d290a77cebe952c8647cbbd3cdfcc423
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-12-07 01:34:24 +0100

    users/1/posts/1 pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,22 @@
 class PostsController < ApplicationController
+  before_action :set_user
+  before_action :set_post, only: [:show]
+
   def index
-    @user = User.find(params[:user_id])
     @posts = @user.posts
+  end
+
+  def show
+    @comments = @post.comments
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_post
+    @post = @user.posts.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   # get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index]
+    resources :posts, only: [:index, :show]
   end
 end
